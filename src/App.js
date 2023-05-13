84b2ebacd1fa9ac7b7c9e0c755375f429c6adeed
Ash Bars <barsegyan@topsystems.ru>
2023-05-13 17:00:57 +0300

    -составить структуру данных услуг, -мапиться по структуре посдтавляя данные слайдера

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
 import "./App.css";
-import Favor from "./assets/Hartavor.jpg";
-import service0 from "./assets/service0.jpg";
-import service1 from "./assets/service1.jpg"
-import service3 from "./assets/service3.jpg"
-import test1 from "./assets/servise4.jpg"
+import Favor from "./_assets/Hartavor.jpg";
+
 import {Hero} from "./components/Hero/Hero";
 import {Slider} from "./components/Slider/Slider";
 import {BasicExample} from "./components/Navbar/HeaderNavBar";
+import {services} from "./_serviceInfo/serviceInfo";
 
 function App() {
-  const navbarLinks = [
-    { url: "#", title: "Home" },
-    { url: "#", title: "Trips" },
-    { url: "#", title: "Rewards" },
-  ];
-
+  const servicesJSX = services.map(t => {
+    return <Slider
+      id={t.id}
+      imageSrc={t.imageSrc}
+      title={t.title}
+      subtitle={t.subtitle}
+      flipped={t.flipped}
+    />
+  });
   return (
     <div className="App">
       <BasicExample/>
       <Hero imageSrc={Favor} />
-      <Slider
-        imageSrc={service0}
-        title={"Be an explorer."}
-        subtitle={
-          "Our platform offers a wide variety of unique travel locations!"
-        }
-      />
-      <Slider
-        imageSrc={service1}
-        title={"Memories for a lifetime."}
-        subtitle={"Your dream vacation is only a few clicks away."}
-        flipped={true}
-      />
-      <Slider
-        imageSrc={service3}
-        title={"Memories for a lifetime."}
-        subtitle={"Your dream vacation is only a few clicks away."}
-
-      />
-      <Slider
-        imageSrc={test1}
-        title={"Memories for a lifetime."}
-        subtitle={"Your dream vacation is only a few clicks away."}
-        flipped={true}
-      />
+      {servicesJSX}
     </div>
   );
 }
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Slider.css";
 import { useInView } from "react-intersection-observer";
+import {ButtonSuccess} from "../elements/button/ButtonSuccess";
 
 export const Slider = ({ imageSrc, title, subtitle, flipped }) => {
   const { ref, inView } = useInView({
     /* Optional options */
     threshold: 0.4,
   });
-
+  console.log('flipped', flipped)
   const renderContent = () => {
     if (!flipped) {
       return (
@@ -16,6 +17,7 @@ export const Slider = ({ imageSrc, title, subtitle, flipped }) => {
           <div className="slider__content">
             <h1 className="slider__title">{title}</h1>
             <p>{subtitle}</p>
+            <ButtonSuccess text={'Узнать больше'}/>
           </div>
         </>
       );
@@ -25,8 +27,10 @@ export const Slider = ({ imageSrc, title, subtitle, flipped }) => {
           <div className="slider__content">
             <h1 className="slider__title">{title}</h1>
             <p>{subtitle}</p>
+            <ButtonSuccess text={'Узнать больше'}/>
           </div>
           <img src={imageSrc} alt="Travel" className="slider__image" />
+
         </>
       );
     }
